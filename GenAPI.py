5afc37ea6c7cce8bd60cb41bca770a5f83ffe886
Or Zellinger <or.zellinger@gmail.com>
2025-09-14 16:52:37 +0300

    complete the class who connect to the GeminiAPI

---
+++ GenAPI.py
@@ -0,0 +1,39 @@
+from google.genai import types
+from google import genai
+import json
+import os
+
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+class GenAPI:
+
+    def __init__(self):
+        self.API_KEY = os.getenv('GEMINI_API_KEY')
+        self.Prompt = os.getenv('PROMPT_HOLDER')
+        self.Client = genai.Client()
+
+
+    def open_image_for_send(self, path_file):
+        with open(path_file, 'rb') as file:
+            image_bytes = file.read()
+
+        return image_bytes
+
+    def get_details_from_gemini(self, image_data):
+        response = self.Client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=[
+                types.Part.from_bytes(
+                    data=image_data,
+                    mime_type='image/jpeg',
+                ),
+                self.Prompt
+            ]
+        )
+        return response.text
+
+    @staticmethod
+    def convert_to_dict(result):
+        return json.loads(result.strip('`').strip('json').strip())
