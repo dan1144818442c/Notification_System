fd6c82d683290f4eb0903f2f70f5ec06e970f1ed
benjyp2003 <benjypfeffer@gmail.com>
2025-09-16 11:44:01 +0300

    Refactor Retriever class to include off-road vehicle check and update data retrieval logic

--- original_details_retriever/app/manager.py
+++ original_details_retriever/app/manager.py
@@ -21,10 +21,10 @@ class Manager:
         self.logger.info(f"Listening for messages from Kafka topic - {self.in_topic} ...")
         for message in self.consumer.consumer:
             try:
-                self.logger.info(f"Received message: {message.value}")
+                self.logger.info(f"\nReceived message: {message.value}")
                 license_plate = message.value.get("number")
                 type = message.value.get("type")
-                message.value["original_details"] = self.retriever.retrieve_cars_data(license_plate, type)
+                message.value["original_details"], message.value["is_off_road"] = self.retriever.retrieve_cars_data(license_plate, type)
 
                 self.logger.info(f"Publishing processed message - {message.value} to kafka topic '{self.out_topic}'...")
                 self.producer.publish_message(self.out_topic, message.value)
